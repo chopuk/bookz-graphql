5e88409fe9e90f38836497cd40d44eaa69bad7b5
Chris Harris <chopuk@gmail.com>
2021-06-10 14:25:10 +0100

    thanks

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'detail/:id', loadChildren: './pages/detail/detail.module#DetailPageModule' },
+  { path: 'add', loadChildren: './pages/add/add.module#AddPageModule' },
+  { path: 'update/:id', loadChildren: './pages/update/update.module#UpdatePageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
